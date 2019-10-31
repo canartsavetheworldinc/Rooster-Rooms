2bef0c0f5d71f561d459fae0806d477e807106e5
tomon9086 <tomo.mouse.9086@gmail.com>
2019-11-01 02:31:12 +0900

    :bug:style scoped

--- src/components/List.vue
+++ src/components/List.vue
@@ -31,7 +31,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 table {
   border-collapse: collapse;
   border-spacing: 0;
--- src/components/ListRow.vue
+++ src/components/ListRow.vue
@@ -25,7 +25,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 tr.header {
   background-color: whitesmoke;
   font-weight: bold;
