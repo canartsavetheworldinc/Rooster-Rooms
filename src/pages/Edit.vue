08bccc400abf897372c8a9bc7875864e65a33adc
tomon9086 <tomo.mouse.9086@gmail.com>
2019-10-31 22:48:21 +0900

    pages

---
+++ src/pages/Edit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>edit</div>
+    <div>{{ data }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HelloWorld from './components/HelloWorld'
 import NotFound from './pages/NotFound'
+import Top from './pages/Top'
+import Edit from './pages/Edit'
 
 Vue.use(Router)
 
@@ -13,7 +14,16 @@ export default new Router({
     {
       path: '/',
       name: 'hello',
-      component: HelloWorld
+      component: Top
+    }, {
+      path: '/edit/:data',
+      name: 'edit',
+      props: true,
+      component: Edit
+    }, {
+      path: '/edit',
+      name: 'edit',
+      component: Edit
     }, {
       path: '*',
       name: 'notfound',
